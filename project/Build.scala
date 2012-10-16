6a5a2417a1fbec7b84becd8ddd27a7c2246b4ee1
Fahied <fahied@gmail.com>
2012-10-16 14:47:58 +0200

    MongoDB Ready
    MongoDB configuration completed

--- project/Build.scala
+++ project/Build.scala
@@ -9,10 +9,22 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       // Add your project dependencies here,
+      
+      "leodagdag"                         % "play2-morphia-plugin_2.9.1"  % "0.0.6"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here     
+      
+      resolvers ++= Seq(DefaultMavenRepository, Resolvers.githubRepository, Resolvers.morphiaRepository),
+    	checksums := Nil 
     )
+    
+    object Resolvers {
+      val githubRepository = "LeoDagDag repository" at "http://leodagdag.github.com/repository/"
+      val dropboxRepository = "Dropbox repository" at "http://dl.dropbox.com/u/18533645/repository/"
+      val morphiaRepository = "Morphia repository" at "http://morphia.googlecode.com/svn/mavenrepo/"
+	}
+	
 
 }
