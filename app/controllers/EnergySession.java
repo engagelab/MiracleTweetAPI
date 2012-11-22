6226945385b74e325d9a93c6338ad64ff34be6a6
Jeremy Toussaint <jt151077@gmail.com>
2012-11-22 09:58:27 +0100

    fixed returned json object for energysession

--- app/controllers/EnergySession.java
+++ app/controllers/EnergySession.java
@@ -7,80 +7,67 @@ import java.util.List;
 import models.EnergySource;
 
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
 
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-public class EnergySession extends Controller{
-	
-	
-	
-	 
-	 public static Result fetchAllEnergySources() 
-	  {
-		 
-		 List<EnergySource> sources = EnergySource.find().all();
+public class EnergySession extends Controller {
+
+	public static Result fetchAllEnergySources() {
+
+		List<EnergySource> sources = EnergySource.find().all();
 
 		return ok(toJson(sources));
-		
-	  }
-	 
-	
-	 public static Result saveSession() 
-	  {
-		 
-		 JsonNode node =  ctx().request().body().asJson();
-		 String energy = node.get("energy").asText();
-		 Boolean inuse   = node.get("inuse").asBoolean();
-		 String token = node.get("token").asText();
-		 
-		 EnergySource source = new EnergySource(energy, inuse, token);
-		 source.insert();
+
+	}
+
+	public static Result saveSession() {
+
+		JsonNode node = ctx().request().body().asJson();
+		String energy = node.get("energy").asText();
+		Boolean inuse = node.get("inuse").asBoolean();
+		String token = node.get("token").asText();
+
+		EnergySource source = new EnergySource(energy, inuse, token);
+		source.insert();
 
 		return ok(toJson(source));
-		
-	  }
-	 
-	 
-	 public static Result updateSession() 
-	  {
-		
-		 JsonNode node =  ctx().request().body().asJson();
-		 String id = node.get("id").asText();
-		 String token = node.get("token").asText();
-		 String stat = node.get("stat").asText();
-		 
-		 EnergySource source = EnergySource.find().filter("id", id).get();
-		 if(source != null) {
-		 	if(stat.equals("pick") && !source.inuse)
-			 {
-				 	source.inuse = true;
-			 		source.token = token;
-			 		source.update();
-					return ok(toJson(source));
-			 }
-			 else if (stat.equals("reset") && source.inuse) {
-					
-		 				source.inuse = false;
-				 		source.token = "";
-				 		source.update();
-					return ok(toJson(source));
+
+	}
+
+	public static Result updateSession() {
+
+		JsonNode node = ctx().request().body().asJson();
+		String id = node.get("id").asText();
+		String token = node.get("token").asText();
+		String stat = node.get("stat").asText();
+
+		EnergySource source = EnergySource.find().filter("id", id).get();
+		if (source != null) {
+			if (stat.equals("pick") && !source.inuse) {
+				source.inuse = true;
+				source.token = token;
+				source.update();
+				return ok(toJson(source));
+			} else if (stat.equals("reset") && source.inuse) {
+
+				source.inuse = false;
+				source.token = "";
+				source.update();
+				return ok(toJson(source));
+			} else {
+				ObjectNode rObj = Json.newObject();
+				rObj.put("inuse", false);
+				return ok(rObj);
 			}
-			else
-			{
-				return ok(toJson("{\"status\":\"inuse\"}"));
-			}
+		} else {
+			ObjectNode rObj = Json.newObject();
+			rObj.put("inuse", false);
+			return ok(rObj);
 		}
-		 else{
-		 	return ok(toJson("{\"status\":\"wrongId\"}"));
-		 }
-		 		
-		
-		
-	  }
-	 
-	 
-	 
-	 
+
+	}
 
 }
